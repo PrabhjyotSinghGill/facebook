8717d3421131d9fd80ff4f4ba01280a30e60d656
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-05-10 16:40:46 -0400

    Header component updated

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Image from "next/image"
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon, 
+} from "@heroicons/react/outline";
 
 
 function Header() {
   return (
     <div>
         {/*Left*/}
-        <div>
-            <Image src="/fb.webp" width={40} height={40}></Image>
+        <div className='flex items-center'>
+            <Image src="/fb.svg" height={40} width={100} layout="fixed"></Image>
+            <div className='flex ml-2 items-center rounded-full'>
+                <SearchIcon className='h-6'></SearchIcon>
+                <input type="text" placeholder='Search Facebook'></input>
+            </div>
         </div>
         {/*Center*/}
         <div></div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
+import Image from "next/image"
 import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Facebook</title>
+        <link rel="shortcut icon" href="fb.webp" />
+        <title >Facebook</title>
       </Head>
       {/*Header */}
       <Header></Header>
