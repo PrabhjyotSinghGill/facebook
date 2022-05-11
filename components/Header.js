93f13cc916c9d9622ccebba4f69b871e22386371
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-05-11 16:26:02 -0400

    header alignment

--- components/Header.js
+++ components/Header.js
@@ -19,18 +19,18 @@ import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-lg'>
         {/*Left*/}
         <div className='flex items-center'>
             <Image src="/fb.webp" height={40} width={40} layout="fixed"></Image>
             <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                 <SearchIcon className='h-6'></SearchIcon>
-                <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type="text" placeholder='Search Facebook'></input>
+                <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder='Search Facebook'></input>
             </div>
         </div>
         {/*Center*/}
-        <div>
-            <div className='flex justify-center flex-grow'>
+        <div className='flex justify-center flex-grow'>
+            <div className='flex space-x-6 md:space-x-2'>
                 <HeaderIcon Icon={HomeIcon}></HeaderIcon>
                 <HeaderIcon Icon={FlagIcon}></HeaderIcon>
                 <HeaderIcon Icon={PlayIcon}></HeaderIcon>
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function HeaderIcon({Icon}) {
-    return <div>
-        <Icon className='h-5'></Icon>
+    return <div className='flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border b-2 active:border-blue-500 group'>
+        <Icon className='h-5 group-hover:text-blue-500'></Icon>
     </div>
 }
 
