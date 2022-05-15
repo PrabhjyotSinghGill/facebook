0815c56fbfe9c2f9fc870f9c34387004cb563ccc
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-05-14 22:03:14 -0400

    right header part added

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,59 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 import {
-    BellIcon,
-    ChatIcon,
-    ChevronDownIcon,
-    HomeIcon,
-    UserGroupIcon,
-    ViewGridIcon,
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
 } from "@heroicons/react/solid";
 import {
-    FlagIcon,
-    PlayIcon,
-    SearchIcon,
-    ShoppingCartIcon, 
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
 } from "@heroicons/react/outline";
-import HeaderIcon from './HeaderIcon';
-
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-lg'>
-        {/*Left*/}
-        <div className='flex items-center'>
-            <Image src="/fb.webp" height={40} width={40} layout="fixed"></Image>
-            <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
-                <SearchIcon className='h-6'></SearchIcon>
-                <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder='Search Facebook'></input>
-            </div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-lg">
+      {/*Left*/}
+      <div className="flex items-center">
+        <Image src="/fb.webp" height={40} width={40} layout="fixed"></Image>
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600"></SearchIcon>
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          ></input>
         </div>
-        {/*Center*/}
-        <div className='flex justify-center flex-grow'>
-            <div className='flex space-x-6 md:space-x-2'>
-                <HeaderIcon Icon={HomeIcon}></HeaderIcon>
-                <HeaderIcon Icon={FlagIcon}></HeaderIcon>
-                <HeaderIcon Icon={PlayIcon}></HeaderIcon>
-                <HeaderIcon Icon={ShoppingCartIcon}></HeaderIcon>
-                <HeaderIcon Icon={UserGroupIcon}></HeaderIcon>
-            </div>
+      </div>
+      {/*Center*/}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon}></HeaderIcon>
+          <HeaderIcon Icon={FlagIcon}></HeaderIcon>
+          <HeaderIcon Icon={PlayIcon}></HeaderIcon>
+          <HeaderIcon Icon={ShoppingCartIcon}></HeaderIcon>
+          <HeaderIcon Icon={UserGroupIcon}></HeaderIcon>
         </div>
-        {/*Right*/}
-        <div></div>
+      </div>
+      {/*Right*/}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+        <p className="whitespace-nowrap font-semibold pr-3">
+          Prabhjyot Singh Gill
+        </p>
+        <ViewGridIcon className="icon"></ViewGridIcon>
+        <ChatIcon className="icon"></ChatIcon>
+        <BellIcon className="icon"></BellIcon>
+        <ChevronDownIcon className="icon"></ChevronDownIcon>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
