b7d748a2fbc195ed4eb31d38ed3bdcaf3bab1af5
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-05-18 09:16:56 -0400

    InputBox component created

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Stories from './Stories';
+import InputBox from './InputBox';
 
 
 function Feed() {
     return <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
         <div className='mx-auto max-w-md md:max-w-lg'>
         <Stories></Stories>
-        {/* InputBox */}
+        <InputBox></InputBox>
         {/* Posts */}
         </div>
     </div>;
