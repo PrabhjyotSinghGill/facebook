ede4d109304b44dee3fc575237b408e707a303c8
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-05-18 13:41:24 -0400

    inputbox component created

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from "next/image";
+import {EmojiHappyIcon} from "@heroicons/react/outline";
+import {CameraIcon, VideoCameraIcon} from "@heroicons/react/solid";
 
 function InputBox() {
     const sendPost = e => {
@@ -13,6 +15,20 @@ function InputBox() {
                 <input className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none" type="text" placeholder={`What's on your mind?`}></input>
                 <button hidden type="submit" onClick={sendPost}>Submit</button>
             </form>
+        </div>
+        <div className='flex justify-evenly p-3 border-top '>
+            <div className='inputIcon'>
+                <VideoCameraIcon className='h-7 text-red-500'></VideoCameraIcon>
+                <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
+            </div>
+            <div className='inputIcon'>
+                <CameraIcon className='h-7 text-green-400'></CameraIcon>
+                <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+            </div>
+            <div className='inputIcon'>
+                <EmojiHappyIcon className='h-7 text-yellow-300'></EmojiHappyIcon>
+                <p className='text-xs sm:text-sm xl:text-base'>Feeling/Activity</p>
+            </div>
         </div>
     </div>;
 }
