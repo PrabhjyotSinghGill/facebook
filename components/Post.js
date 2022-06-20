eb6cd46bb88c84709a99006d0becd83a2ae3662e
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-06-20 11:19:20 -0400

    Post modified

--- components/Post.js
+++ components/Post.js
@@ -1,18 +1,30 @@
 import React from "react";
+import Image from "next/image";
 
 function Post({ name, message, email, timestamp, image, postImage }) {
   return (
     <div className="flex flex-col">
-      <div>
-        <div>
-          <img
+      <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+        <div className="flex items-center space-x-2">
+          <Image
             className="rounded-full"
             src={image}
             width={40}
             height={40}
             alt=""
-          ></img>
-          <div></div>
+          ></Image>
+          <div>
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+               {
+                timestamp
+               } 
+            </p>
+          </div>
+        </div>
+        <p className="pt-4">{message}</p>
+        <div className="relative h-56 md:h-96 bg-white ">
+            <Image src={postImage} objectFit='cover' layout="fill"></Image>
         </div>
       </div>
     </div>
