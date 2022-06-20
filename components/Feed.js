bcec5c4a4cab549208ea5bbdf6f9b2f977eb5b05
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-06-20 14:33:11 -0400

    Widgets Responsive Component complete

--- components/Feed.js
+++ components/Feed.js
@@ -3,8 +3,9 @@ import Stories from './Stories';
 import InputBox from './InputBox';
 import Posts from "./Posts";
 
+
 function Feed() {
-    return <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
+    return <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide'>
         <div className='mx-auto max-w-md md:max-w-lg'>
         <Stories></Stories>
         <InputBox></InputBox>
--- components/Widgets.js
+++ components/Widgets.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {SearchIcon} from "@heroicons/react/outline";
+import {DotsHorizontalIcon, VideoCameraIcon} from "@heroicons/react/solid";
+import Contact from "./Contact";
+
 const contacts = [
     {src:"/a.jpg" , name:"Jeff Bezoz"},
     {src:"/b.jpg" , name:"Elon Musk"},
@@ -6,12 +10,22 @@ const contacts = [
     {src:"/d.jpg" , name:"Maek Zuckerberg"},
     {src:"/e.jpg" , name:"Harry Potter"},
     {src:"/f.jpg" , name:"The Queen"},
-    {src:"/g.jpg" , name:"James Bond"},
+    {src:"/h.jpg" , name:"James Bond"},
 ];
 
 
 function Widgets() {
-    return <div></div>;
+    return <div className='hidden lg:flex flex-col w-60 p-2 mt-5' >
+        <div className='flex justify-between items-center text-gray-500 mb-5'>
+            <h2 className="text-xl">Contacts</h2>
+            <div className="flex space-x-2">
+                <VideoCameraIcon className="h-6"/>
+                <SearchIcon className="h-6"/>
+                <DotsHorizontalIcon className="h-6" />
+            </div>
+        </div>
+        {contacts.map(contact => (<Contact key={contact.src} src={contact.src} name={contact.name}></Contact>))}
+    </div>;
 }
 
 export default Widgets;
