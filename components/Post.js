20c047a7582b33e48e39bb7a25bff18d5c0392ef
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-06-20 13:26:53 -0400

    Post component complete

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import {ChatAltIcon, ShareIcon, ThumbUpIcon} from "@heroicons/react/outline";
+
 
 function Post({ name, message, email, timestamp, image, postImage }) {
   return (
@@ -23,9 +25,25 @@ function Post({ name, message, email, timestamp, image, postImage }) {
           </div>
         </div>
         <p className="pt-4">{message}</p>
+        {postImage && (
         <div className="relative h-56 md:h-96 bg-white ">
             <Image src={postImage} objectFit='cover' layout="fill"></Image>
+        </div>)}
+        <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+            <div className="inputIcon rounded-none rounded-bl-2xl">
+                <ThumbUpIcon className="h-4"></ThumbUpIcon>
+                <p className="text-xs sm:text-base">Like</p>
+            </div>
+            <div className="inputIcon rounded-none">
+                <ChatAltIcon className="h-4"></ChatAltIcon>
+                <p className="text-xs sm:text-base">Comment</p>
+            </div>
+            <div className="inputIcon rounded-none rounded-br-2xl">
+                <ShareIcon className='h-4'></ShareIcon>
+                <p className="text-xs sm:text-base">Share</p>
+            </div>
         </div>
+
       </div>
     </div>
   );
