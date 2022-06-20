e03e855adaa11ebb42dd2a55c50bfea31e71c62b
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-06-20 13:51:46 -0400

    Widgets Component Modified

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,4 +1,14 @@
 import React from 'react';
+const contacts = [
+    {src:"/a.jpg" , name:"Jeff Bezoz"},
+    {src:"/b.jpg" , name:"Elon Musk"},
+    {src:"/c.jpg" , name:"Bill Gates"},
+    {src:"/d.jpg" , name:"Maek Zuckerberg"},
+    {src:"/e.jpg" , name:"Harry Potter"},
+    {src:"/f.jpg" , name:"The Queen"},
+    {src:"/g.jpg" , name:"James Bond"},
+];
+
 
 function Widgets() {
     return <div></div>;
