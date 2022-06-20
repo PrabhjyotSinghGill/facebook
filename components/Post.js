0a664a092d8ccf3e6f0d71948ba00917c69533e7
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-06-19 21:20:02 -0400

    Post component modified

--- components/Post.js
+++ components/Post.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function Post({ name,
-message,
-email,
-timestamp,
-image,
-postImage}) {
-    return <div className='post'>
-        <h1>{name}</h1>
+function Post({ name, message, email, timestamp, image, postImage }) {
+  return (
+    <div className="flex flex-col">
+      <div>
+        <div>
+          <img
+            className="rounded-full"
+            src={image}
+            width={40}
+            height={40}
+            alt=""
+          ></img>
+          <div></div>
+        </div>
+      </div>
     </div>
+  );
 }
 
 export default Post;
