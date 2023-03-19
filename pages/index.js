723cd438feaef7f8dac2843360809997b659f5fb
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-03-19 13:26:21 -0700

    github pages : fbdeploy.yml file altered

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from "next/image"
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Feed from './../components/Feed';
